fa85b409439e1b4ff12536a7054373ebe903093e
Chrisnox-209 <89594242+Chrisnox-209@users.noreply.github.com>
2026-07-01 20:56:48 +0200

    🧩 feat(MAIN): Ajout de la fonction check_argument (argparse)

--- src/main.py
+++ src/main.py
@@ -1,14 +1,30 @@
 from parse import json_to_data, ParsingPompt, ParsngFunctions
+import argparse
+from typing import Any
 
 
-def main() -> None:
-    data_prompt = json_to_data('../data/input/function_calling_tests.json')
-    data_function = json_to_data('../data/input/functions_definition.json')
+def check_argument() -> tuple[Any, Any, Any]:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input",
+                        default="data/input/function_calling_tests.json")
+    parser.add_argument("--output",
+                        default="data/output/function_calls.json")
+    parser.add_argument("--functions_definition",
+                        default="data/input/functions_definition.json")
+    args: argparse.Namespace = parser.parse_args()
+    return args.input, args.output, args.functions_definition
+
+
+def main(input: str, output: str, functions_definition: str) -> None:
+    data_prompt: list[dict[str, Any]] = json_to_data(input)
+    data_function: list[dict[str, Any]] = json_to_data(functions_definition)
     ParsingPompt.parse_prompts(data_prompt)
     ParsngFunctions.parse_functions(data_function)
 
 
 if __name__ == "__main__":
-    # if len(sys.argv) > 1:
-    #     print("ok")
-    main()
+    input: str
+    output: str
+    functions_definition: str
+    input, output, functions_definition = check_argument()
+    main(input, output, functions_definition)
