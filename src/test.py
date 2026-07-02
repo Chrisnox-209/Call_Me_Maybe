3d2b9aa5218c34d0490e92dde04ffe8351c568c2
Chrisnox-209 <89594242+Chrisnox-209@users.noreply.github.com>
2026-07-02 17:41:45 +0200

    💄 style(PEP8/MYPY): Mise a la norme

--- src/test.py
+++ src/test.py
@@ -1,37 +1,43 @@
-from llm_sdk import Small_LLM_Model
+from llm_sdk.llm_sdk import Small_LLM_Model
+from typing import Any
 
-def main():
-    print("⏳ Chargement du modèle...")
-    model = Small_LLM_Model()
 
-    prompt = "The capital of France is"
-    print(f"\n🗣️ Prompt de départ : '{prompt}'")
-    
+def main() -> None:
+    print("Chargement du modèle...")
+    llm: Any = Small_LLM_Model(device="cpu")
+
+    prompt = "La capitale de la France est"
+    print(f"Prompt de départ : '{prompt}'")
+
     # 1. ENCODAGE : On traduit le texte en liste de numéros (Input IDs)
-    input_ids = model.encode(prompt)[0].tolist()
-    print(f"🔢 Traduction pour l'IA (Input IDs) : {input_ids}")
-    
-    print("\n🤖 Génération en cours (5 mots max)...")
-    
+    input_ids: Any = llm.encode(prompt)[0].tolist()
+    print(f"Traduction pour l'IA (Input IDs) : {input_ids}")
+
+    print("Génération en cours (5 mots max)...")
     # 2. BOUCLE D'INFÉRENCE
-    for etape in range(5):
+    for etape in range(20):
         # A. On demande les probabilités pour le prochain numéro
-        logits = model.get_logits_from_input_ids(input_ids)
-        
+        logits: list[float] = llm.get_logits_from_input_ids(input_ids)
+
         # B. On choisit la probabilité la plus haute (le meilleur token)
-        meilleure_proba = max(logits)
-        prochain_token_id = logits.index(meilleure_proba)
-        
+        meilleure_proba: float = max(logits)
+        prochain_token_id: int = logits.index(meilleure_proba)
+        if prochain_token_id == 151643:
+            print("Signal de fin")
+            break
+
         # C. On ajoute ce nouveau numéro à notre liste pour le tour suivant
         input_ids.append(prochain_token_id)
-        
+
         # D. On décode juste ce token pour l'afficher à l'écran
-        mot = model.decode([prochain_token_id])
+        mot: Any = llm.decode([prochain_token_id])
+        print(f"Meilleur proba: {meilleure_proba}")
         print(f"   ↳ Token {prochain_token_id} -> '{mot}'")
 
     # 3. DÉCODAGE FINAL : On retransforme tous les numéros en texte lisible
-    reponse_complete = model.decode(input_ids)
+    reponse_complete: Any = llm.decode(input_ids)
     print(f"\n✅ Résultat final : {reponse_complete}")
+
 
 if __name__ == "__main__":
     main()
