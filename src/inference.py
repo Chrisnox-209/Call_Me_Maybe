3d5d79fbf2cd802d78b4255ddac299ba2a41ce8b
Chrisnox-209 <christophe.pietrzak@gmail.com>
2026-07-03 22:29:49 +0200

    🚧 wip(TEST/INFERENCE): Travail en cours ..

--- src/inference.py
+++ src/inference.py
@@ -1,4 +1,75 @@
-from llm_sdk import Small_LLM_Model 
-import json
+from llm_sdk import Small_LLM_Model  # type: ignore
+from parse import ParsingPompt, ParsngFunctions
+from typing import Any, Iterator, Literal
+# import string
+# import json
 
-def 
+
+def build_prompt_func(data_function: list[ParsngFunctions]) -> str:
+    prompt_func: str = ''
+    for func in data_function:
+        prompt_func = prompt_func + f"name: {func.name}\n"
+        prompt_func = prompt_func + f"description: {func.description}\n"
+        prompt_func = prompt_func + "parameters:\n"
+        for name, param in func.parameters.items():
+            prompt_func = prompt_func + f"  - {name}: {param.type}\n"
+        prompt_func = prompt_func + f"returns: {func.returns.type}\n\n"
+    return prompt_func
+
+
+def build_struct() -> str:
+    return 'result = [{"prompt": ...,"name": ...,"parameters": {...}}, ...'
+
+
+def run_inference(data_prompt: list[ParsingPompt],
+                  data_function: list[ParsngFunctions],
+                  output: str) -> None:
+    llm: Any = Small_LLM_Model()
+
+    # prompts: Iterator[ParsingPompt] = iter(data_prompt)
+    prompt_func: str = build_prompt_func(data_function)
+
+#    for i in range(10):
+#    while True:
+
+#        if j != 0:
+    # Decode
+
+    # text: ParsingPompt = next(prompts)
+    # prompt: str = text.prompt
+    pre_prompt = ('You are an assistant. Here is a question. Here are the '
+                  'functions. Complete the rest with the correct information '
+                  'to generate a JSON format.\n\n[{"prompt":')
+
+    # struct: str = build_struct()
+    llm_prompt: str = (
+        "What is the sum of 2 and 3?\n\n" + prompt_func + pre_prompt)
+    exit_llm: list[int] = []
+
+    # ENCODAGE
+    input_ids: Any = llm.encode(llm_prompt)[0].tolist()
+
+    for etape in range(20):
+
+        # A. On demande les probabilités pour le prochain numéro
+        logits: list[float] = llm.get_logits_from_input_ids(input_ids)
+
+        # B. On choisit la probabilité la plus haute (le meilleur token)
+        meilleure_proba: float = max(logits)
+        prochain_token_id: int = logits.index(meilleure_proba)
+        if prochain_token_id == 151643:
+            print("Signal de fin")
+            break
+
+        # C. On ajoute ce nouveau numéro à notre liste pour le tour suivant
+        exit_llm.append(prochain_token_id)
+        input_ids.append(prochain_token_id)
+
+        # D. On décode juste ce token pour l'afficher à l'écran
+        mot: Any = llm.decode([prochain_token_id])
+        # print(f"Meilleur proba: {meilleure_proba}")
+        # print(f"   ↳ Token {prochain_token_id} -> '{mot}'")
+    # 3. DÉCODAGE FINAL : On retransforme tous les numéros en texte lisible
+
+    reponse_complete: Any = llm.decode(exit_llm)
+    print(f"\n✅ Résultat final : {reponse_complete}")
--- src/test.py
+++ src/test.py
@@ -1,42 +1,78 @@
 from llm_sdk import Small_LLM_Model  # type: ignore
 from typing import Any
+import numpy as np
+from numpy.typing import NDArray
+# import json
 
 
 def main() -> None:
-    print("Chargement du modèle...")
     llm: Any = Small_LLM_Model(device="cpu")
 
-    prompt = "La capitale de la France est"
-    print(f"Prompt de départ : '{prompt}'")
+    questions: list[str] = [
+        "Est-ce que le soleil est une étoile ?",
+        "Est-ce que l’eau est liquide à température ambiante ?",
+        "Est-ce que Paris est en France ?",
+        "Est-ce que 2 est un nombre pair ?",
+        "Est-ce que les humains respirent de l’oxygène ?",
+        "Est-ce que la Terre est une planète ?",
+        "Est-ce que la Lune tourne autour de la Terre ?",
+        "Est-ce que le feu produit de la chaleur ?",
+        "Est-ce que 10 est plus grand que 5 ?",
+        "Est-ce que les chats sont des animaux ?"
+    ]
 
-    # 1. ENCODAGE : On traduit le texte en liste de numéros (Input IDs)
-    input_ids: Any = llm.encode(prompt)[0].tolist()
-    print(f"Traduction pour l'IA (Input IDs) : {input_ids}")
+    # path_vocab: str = llm.get_path_to_vocab_file()
+    # with open(path_vocab, "r", encoding="utf-8") as file:
+    #     vocabulaire: Any = json.load(file)
 
-    print("Génération en cours (5 mots max)...")
-    # 2. BOUCLE D'INFÉRENCE
-    for etape in range(20):
-        # A. On demande les probabilités pour le prochain numéro
-        logits: list[float] = llm.get_logits_from_input_ids(input_ids)
+    tokens_autorises_ids: list[Any] = [
+        llm.encode("oui")[0][0],
+        llm.encode("non")[0][0]
+    ]
 
-        # B. On choisit la probabilité la plus haute (le meilleur token)
-        meilleure_proba: float = max(logits)
-        prochain_token_id: int = logits.index(meilleure_proba)
-        if prochain_token_id == 151643:
-            print("Signal de fin")
-            break
+    for i in range(len(questions)):
 
-        # C. On ajoute ce nouveau numéro à notre liste pour le tour suivant
-        input_ids.append(prochain_token_id)
+        # Prompt système simple
+        # pre_prompt = (
+        #     "Tu es un modèle d’IA et tu dois répondre "
+        #     "uniquement par oui ou par non."
+        # )
+        pre_prompt = (
+            "Réponds uniquement selon la vérité factuelle.\n"
+            "Réfléchis avant de répondre."
+        )
 
-        # D. On décode juste ce token pour l'afficher à l'écran
-        mot: Any = llm.decode([prochain_token_id])
-        print(f"Meilleur proba: {meilleure_proba}")
-        print(f"   ↳ Token {prochain_token_id} -> '{mot}'")
+        prompt: str = pre_prompt + "\n\n" + questions[i]
 
-    # 3. DÉCODAGE FINAL : On retransforme tous les numéros en texte lisible
-    reponse_complete: Any = llm.decode(input_ids)
-    print(f"\n✅ Résultat final : {reponse_complete}")
+        # 6. Encodage du prompt
+        input_ids: Any = llm.encode(prompt)[0].tolist()
+
+        # 7. Récupération des logits
+        logits: NDArray[Any] = np.array(llm.get_logits_from_input_ids(
+            input_ids))
+
+        # 8. MASKING DES LOGITS
+        # On met tous les tokens à -inf sauf oui/non
+        masked_logits: NDArray[Any] = np.full_like(logits, -np.inf)
+
+        # On garde uniquement les logits des tokens autorisés
+        masked_logits[tokens_autorises_ids] = logits[tokens_autorises_ids]
+        for t in tokens_autorises_ids:
+            print(t, logits[t])
+
+        # 9. Sélection du meilleur token
+        prochain_token_id = int(np.argmax(masked_logits))
+
+        # 10. Décodage du token
+        result: Any = llm.decode(prochain_token_id)
+
+        if "oui" in result.lower():
+            color = "\033[32m"
+        elif "non" in result.lower():
+            color = "\033[31m"
+        else:
+            color = "\033[0m"
+        print(f"{color}[{result}]\033[0m --> {questions[i]}")
 
 
 if __name__ == "__main__":
