75223d70a1e298a67d3a80afa35ba40752dfb53a
Chrisnox-209 <89594242+Chrisnox-209@users.noreply.github.com>
2026-08-16 17:52:45 +0200

    🧩 feat(UTILS): Ajout de l'enum Color

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,19 @@
 from parse import Color, ParsngFunctions
 import json
 from typing import Any
+from enum import Enum
+
+
+class Color(Enum):
+    RESET = "\033[0m"
+    RED = "\033[91m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    BLUE = "\033[94m"
+    MAGENTA = "\033[95m"
+    CYAN = "\033[96m"
+    WHITE = "\033[97m"
+    ORANGE = "\033[38;5;208m"
 
 
 def charge_vocab(llm: Any) -> Any:
